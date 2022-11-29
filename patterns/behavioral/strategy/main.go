f0c2d283f89bd7a5b2e0978352729a0fbc316b05
Dias12 <diasoryntayev@gmail.com>
2022-11-29 20:11:10 +0600

    add main

---
+++ patterns/behavioral/strategy/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "study/patterns/behavioral/strategy/pkg"
+
+var (
+	start      = 10
+	end        = 100
+	strategies = []pkg.Strategy{
+		&pkg.PublicTransportStrategy{},
+		&pkg.RoadStrategy{},
+		&pkg.WalkStrategy{},
+	}
+)
+
+func main() {
+	nav := pkg.Navigator{}
+	for _, strategy := range strategies {
+		nav.SetStrategy(strategy)
+		nav.Route(start, end)
+	}
+}
