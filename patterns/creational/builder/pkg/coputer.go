b6842474ea23dab80d82ab2a3d21acdcc51671ab
Dias12 <diasoryntayev@gmail.com>
2022-12-07 15:57:06 +0600

    add collector

---
+++ patterns/creational/builder/pkg/coputer.go
@@ -0,0 +1,16 @@
+package pkg
+
+import "fmt"
+
+type Computer struct {
+	Core        int
+	Brand       string
+	Memory      int
+	Monitor     int
+	GraphicCard int
+}
+
+func (pc *Computer) Print() {
+	fmt.Printf("%s Core:[%d] Mem:[%d] BraphicCard:[%d] Monitor:[%d]\n",
+		pc.Brand, pc.Core, pc.Memory, pc.GraphicCard, pc.Monitor)
+}
